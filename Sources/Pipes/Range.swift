d8dc36f16f64ebd51c64a0658f149fc344026100
Craig Edwards <craig@blackdogfoundry.com>
2020-11-11 09:17:24 +1100

    Column range extraction

--- Sources/Pipes/Range.swift
+++ Sources/Pipes/Range.swift
@@ -1,10 +1,39 @@
 import Foundation
 
+extension Int {
+    static public var end = Int.max
+}
 public indirect enum PipeRange {
     case full
     case column(start: Int, end: Int)
     case word(start: Int, end: Int, separator: Character = " ")
     case field(start: Int, end: Int, separator: Character = "\t")
+
+    fileprivate static func resolve(start originalStart: Int, end originalEnd: Int, length: Int) throws -> (start: Int, end: Int)? {
+        // Our indexes are 1-based, so we can do some basic validation here.
+        guard originalStart != 0 && originalEnd != 0 else {
+            throw PipeError.invalidRange(range: "\(originalStart);\(originalEnd)")
+        }
+
+        // Resolve Int.end
+        let start = originalStart == .end ? 1 : originalStart
+        let end = originalEnd
+
+        // Resolve negative offsets
+        let resolvedStart = start > 0 ? start : length + start + 1
+        let resolvedEnd = end > 0 ? end : length + end + 1
+        if resolvedStart > resolvedEnd {
+            throw PipeError.invalidRange(range: "\(originalStart);\(originalEnd)")
+        }
+
+        // If the start is beyond the end, there's no point returning sensible
+        // indexes, so we just return nil.
+        if resolvedStart > length {
+            return nil
+        } else {
+            return (start: max(1, min(length, resolvedStart)) - 1, end: max(1, min(length, resolvedEnd)))
+        }
+    }
 }
 
 extension String {
@@ -12,29 +41,14 @@ extension String {
         switch range {
         case .full:
             return self
-        case .column(let start, let end):
-            // Start cannot be negative and our indexes are 1-based, so
-            // we can do some basic validation here.
-            guard start > 0 && end != 0 else {
-                throw PipeError.invalidRange(range: "\(start);\(end)")
-            }
-
-            // Resolve negative offsets
-            let resolvedStart = start
-            let resolvedEnd = end > 0 ? end : self.count + end + 1
-            if resolvedStart > resolvedEnd {
-                throw PipeError.invalidRange(range: "\(start);\(end)")
-            }
-
-            // If the start is beyond the end of the string, we know we're
-            // not going to be able to extract anything
-            if resolvedStart > self.count {
+        case .column(let originalStart, let originalEnd):
+            guard let (start, end) = try PipeRange.resolve(start: originalStart, end: originalEnd, length: self.count) else {
                 return ""
             }
 
             // Now let's convert our start/end into indexes into the string
-            let startIndex = self.index(self.startIndex, offsetBy: max(1, min(self.count, resolvedStart)) - 1)
-            let endIndex = self.index(self.startIndex, offsetBy: max(1, min(self.count, resolvedEnd)))
+            let startIndex = self.index(self.startIndex, offsetBy: start)
+            let endIndex = self.index(self.startIndex, offsetBy: end)
 
             return String(self[startIndex..<endIndex])
         default:
--- Tests/PipesTests/RangeTests.swift
+++ Tests/PipesTests/RangeTests.swift
@@ -2,21 +2,33 @@ import XCTest
 @testable import Pipes
 
 final class RangeTests: XCTestCase {
-    func testRangeExtract() throws {
+    func testRangeExtractColumn() throws {
         XCTAssertThrows(try "".extract(fromRange: PipeRange.column(start: 0, end: 1)), PipeError.invalidRange(range: "0;1"))
         XCTAssertThrows(try "".extract(fromRange: PipeRange.column(start: 2, end: 1)), PipeError.invalidRange(range: "2;1"))
         XCTAssertThrows(try "".extract(fromRange: PipeRange.column(start: -2, end: -3)), PipeError.invalidRange(range: "-2;-3"))
 
+        XCTAssertEqual(try "".extract(fromRange: PipeRange.column(start: 2, end: 2)), "")
         XCTAssertEqual(try "abcdefghij".extract(fromRange: PipeRange.column(start: 2, end: 2)), "b")
         XCTAssertEqual(try "abcdefghij".extract(fromRange: PipeRange.column(start: 2, end: 4)), "bcd")
         XCTAssertEqual(try "abcdefghij".extract(fromRange: PipeRange.column(start: 1, end: 10)), "abcdefghij")
         XCTAssertEqual(try "abcdefghij".extract(fromRange: PipeRange.column(start: 1, end: 100)), "abcdefghij")
         XCTAssertEqual(try "abcdefghij".extract(fromRange: PipeRange.column(start: 99, end: 100)), "")
         XCTAssertEqual(try "abcdefghij".extract(fromRange: PipeRange.column(start: 3, end: -3)), "cdefgh")
+        XCTAssertEqual(try "abcdefghij".extract(fromRange: PipeRange.column(start: -5, end: -3)), "fgh")
+
+        XCTAssertEqual(try "abcdefghij".extract(fromRange: PipeRange.column(start: .end, end: 3)), "abc")
+        XCTAssertEqual(try "abcdefghij".extract(fromRange: PipeRange.column(start: 3, end: .end)), "cdefghij")
+        XCTAssertEqual(try "abcdefghij".extract(fromRange: PipeRange.column(start: .end, end: .end)), "abcdefghij")
 
         XCTAssertEqual(try "abcde".extract(fromRange: PipeRange.column(start: 2, end: -2)), "bcd")
         XCTAssertThrows(try "abcde".extract(fromRange: PipeRange.column(start: 2, end: -5)), PipeError.invalidRange(range: "2;-5"))
         XCTAssertEqual(try "abcdefgh".extract(fromRange: PipeRange.column(start: 2, end: -5)), "bcd")
+
+        // TODO range substrings
+        // String s = "abcdefghijklmnopqrstuvwxyz";
+        // assertEquals("abcdefghijklm", scanRange(new PipeArgs("1-13 "), true).extractRange(s));
+        // assertEquals("cdefghij", scanRange(new PipeArgs("SUBSTR 3.8 OF 1-13 "), true).extractRange(s));
+        // assertEquals("defg", scanRange(new PipeArgs("SUBSTR 2-5 OF SUBSTR 3.8 OF 1-13 "), true).extractRange(s));
     }
 
     func testSimpleMatches() throws {
@@ -41,7 +53,8 @@ final class RangeTests: XCTestCase {
         XCTAssertEqual(try "abcdef".matches("DUY", anyCase: true, anyOf: true), true)
         XCTAssertEqual(try "abcdef".matches("DUY", anyCase: true, anyOf: false), false)
     }
-    func testColumnRanges() throws {
+
+    func testColumnMatches() throws {
         XCTAssertEqual(try "abcdefghij".matches("b", inRange: .column(start: 2, end: 2)), true)
         XCTAssertEqual(try "abcdefghij".matches("bc", inRange: .column(start: 2, end: 2)), false)
         XCTAssertEqual(try "abcdefghij".matches("b", inRange: .column(start: 2, end: 4)), true)
@@ -56,11 +69,11 @@ final class RangeTests: XCTestCase {
         XCTAssertEqual(try "abcdefghij".matches("BCD", inRange: .column(start: 2, end: 4), anyCase: true), true)
     }
 
-    func testWordRanges() throws {
+    func testWordMatches() throws {
 
     }
 
-    func testFieldRanges() throws {
+    func testFieldMatches() throws {
 
     }
 }
